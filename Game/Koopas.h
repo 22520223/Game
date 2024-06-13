2d7c2227dc5fb8cb4795033c59752f63226054e3
DatNT1507 <nguyenthanhdat15072004@gmail.com>
2024-06-13 16:03:51 +0700

    Change IsBlocking Koopas

--- Game/Koopas.h
+++ Game/Koopas.h
@@ -44,7 +44,7 @@ protected:
 	virtual void Render();
 
 	virtual int IsCollidable() { return 1; };
-	virtual int IsBlocking() { return state == KOOPAS_STATE_KICK_LEFT || state == KOOPAS_STATE_KICK_RIGHT; }
+	virtual int IsBlocking() { return 1; }
 	virtual void OnNoCollision(DWORD dt);
 
 	virtual void OnCollisionWith(LPCOLLISIONEVENT e);
